9d13529440cd955b3cfb5c4281c9eafdc663c3a2
unimmi <defichel@gmail.com>
2016-12-20 17:40:10 +0300

    project

---
+++ assets/php/callback.php
@@ -0,0 +1,16 @@
+<?php
+if($_POST)
+{
+    
+    /*require_once "database.php";
+
+    define('MYSQL_TABLE', 'call_back');
+
+    $conn = db_connect();
+    insertRequest($conn, $_GET['fname'], $_GET['ftele'], MYSQL_TABLE,'dima','wew');
+*/
+   
+
+   echo('Скрипт сработал');
+}
+?>
---
+++ assets/php/save.php
@@ -0,0 +1,10 @@
+﻿<?php
+
+$name = htmlspecialchars($_POST["name"]);
+$email = htmlspecialchars($_POST["email"]);
+$message = htmlspecialchars($_POST["message"]);
+
+		$link = mysqli_connect('localhost', 'root', '', 'basename') or die('Cant connect: ');
+		$query = "INSERT INTO `site` VALUES ('$name','$email','$message')";
+		mysqli_query($link, $query) or die('Cant query: ');
+?>
